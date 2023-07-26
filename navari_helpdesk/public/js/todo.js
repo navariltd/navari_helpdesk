4a240a2ee939934a0a3ae0c71b6045ad91a1f2b9
Lucky-Tsuma <tsumahmumba@gmail.com>
2023-07-26 03:17:07 +0300

    fix: get receiver list, send sms, todo doctype

--- navari_helpdesk/public/js/todo.js
+++ navari_helpdesk/public/js/todo.js
@@ -17,54 +17,64 @@ frappe.ui.form.on('ToDo', {
     }
 });
 
-const send_sms = async function (frm) {
-    let phone_number_list = await frappe.call({
+let send_sms = function (frm) {
+    frappe.call({
         method: "navari_helpdesk.controllers.receiver_list_utils.strip_phone_numbers",
         args: {
             'receiver_list': frm.doc.receiver_list
+        },
+        callback: function (response) {
+            if (response.message) {
+                if (changed_field == 'allocated_to') {
+                    message = ' Issue Type: ' + frm.doc.issue_type  + ' @ ' + frm.doc.location + ', of Priority: ' + frm.doc.priority + ', has been assigned to you';
+                } else if (changed_field == 'department') {
+                    message = ' Issue Type: ' + frm.doc.issue_type  + ' @ ' + frm.doc.location + ' of Priority: ' + frm.doc.priority + ', has been moved to your department';
+                }  
+
+                frappe.call({
+                    method: "frappe.core.doctype.sms_settings.sms_settings.send_sms",
+                    args: {
+                        receiver_list: response.message,
+                        msg: message,
+                    },
+                    callback: function (r) {
+                        if (r.exc) {
+                            msgprint(r.exc);
+                        }
+                    }
+                });
+                
+            }
         }
     })
-    
-    if (phone_number_list) {
-        if (changed_field == 'allocated_to') {
-            message = ' Issue Type: ' + frm.doc.issue_type  + ' @ ' + frm.doc.location + ', of Priority: ' + frm.doc.priority + ', has been assigned to you';
-        } else if (changed_field == 'department') {
-            message = ' Issue Type: ' + frm.doc.issue_type  + ' @ ' + frm.doc.location + ' of Priority: ' + frm.doc.priority + ', has been moved to your department';
-        }
-
-        frappe.call({
-            method: "frappe.core.doctype.sms_settings.sms_settings.send_sms",
-            args: {
-                receiver_list: phone_number_list,
-                msg: message,
-            },
-            callback: function (r) {
-                if (r.exc) {
-                    msgprint(r.exc);
-                }
-            }
-        });
-    }
 }
 
-const get_department_receivers_list = async function (frm) {
-    let receivers = await frappe.call({
+let get_department_receivers_list = function (frm) {
+    frappe.call({
             method: "navari_helpdesk.controllers.receiver_list_utils.get_department_receiver_list",
             args: {
                 'department': frm.doc.department
+            },
+            callback: function (response) {
+               if (response.message) {
+                   frm.set_value('receiver_list', response.message)
+               } 
             }
         })
         
-    frm.set_value('receiver_list', receivers);
 }
 
-const get_issue_assignees_list = async function (frm) {
-    let receivers = await frappe.call({
+let get_issue_assignees_list = function (frm) {
+    frappe.call({
             method: "navari_helpdesk.controllers.receiver_list_utils.get_allocated_to_receiver_list",
             args: {
                 'allocated_to': frm.doc.allocated_to
+            },
+            callback: function (response) {
+                if (response.message) {
+                    frm.set_value('receiver_list', response.message)     
+                }
             }
         })
         
-    frm.set_value('receiver_list', receivers);
 }
